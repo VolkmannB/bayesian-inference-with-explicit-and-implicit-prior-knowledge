afc74c380d05b0fdf396678502eaaef2537a1335
Björn Volkmann <bjoern.volkmann@imes.uni-hannover.de>
2023-11-02 13:34:54 +0100

    added ensemble GP

--- GP_Test.py
+++ GP_Test.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 
 
-from src.RGP import ApproximateGP, GaussianRBF
+from src.RGP import ApproximateGP, GaussianRBF, EnsambleGP
 
 
 
@@ -21,10 +21,19 @@ H = GaussianRBF(
     lengthscale=np.array([0.5])
 )
 
-model = ApproximateGP(
+model1 = ApproximateGP(
     basis_function=H,
     w0=np.zeros(inducing_points.size),
     cov0=np.eye(inducing_points.size)*10**2,
+    error_cov=0.5**2
+)
+
+
+model2 = EnsambleGP(
+    basis_function=H,
+    w0=np.zeros(inducing_points.size),
+    cov0=np.eye(inducing_points.size)*10**2,
+    N=50,
     error_cov=0.5**2
 )
 
@@ -33,24 +42,37 @@ rng = np.random.default_rng()
 for i in tqdm(range(1000)):
     X_train = rng.uniform(-10,10,(20,1))
     Y_train = test_function(X_train) + rng.normal(0, 1, X_train.shape)
-    model.update(X_train, Y_train, np.ones((X_train.size,)))
+    model1.update(X_train, Y_train, np.ones((X_train.size,)))
+    model2.update(X_train, Y_train, np.ones((X_train.size,)))
 
 
 
 ################################################################################
 
 
-fig, ax = plt.subplots(1,1)
+fig, ax = plt.subplots(3,1, layout='tight')
 
 
 Xnew = np.arange(-10,10,0.1)
 F = test_function(Xnew)
-m_F, p_F = model.predict(Xnew.reshape((Xnew.size,1)))
+m_F, p_F = model1.predict(Xnew.reshape((Xnew.size,1)))
 s = np.sqrt(np.diag(p_F))
 
-ax.plot(Xnew, F, label='True Function')
-ax.plot(Xnew, m_F.flatten(), linestyle='--', label='GP mean')
-ax.fill_between(Xnew, m_F.flatten() - s, m_F.flatten() + s, color='b', alpha=0.2)
+F_pred = model2.predict(Xnew.reshape((Xnew.size,1)))
+F_pred_mean = F_pred.mean(axis=1)
+
+ax[0].plot(Xnew, F, label='True Function')
+ax[0].plot(Xnew, m_F.flatten(), linestyle='--', color='orange', label='GP mean')
+ax[0].fill_between(Xnew, m_F.flatten() - s, m_F.flatten() + s, color='orange', alpha=0.2)
+
+ax[1].plot(Xnew, F, label='True Function')
+ax[1].plot(Xnew, F_pred, color='orange', alpha=5/model2.W.shape[0])
+ax[1].plot(Xnew, F_pred_mean.flatten(), linestyle='--', color='orange', label='GP mean')
+
+var = np.var(F_pred, axis=1)
+ax[2].plot(Xnew, F, label='True Function')
+ax[2].plot(Xnew, F_pred_mean.flatten(), linestyle='--', color='orange', label='GP mean')
+ax[2].fill_between(Xnew, F_pred_mean - np.sqrt(var), F_pred_mean + np.sqrt(var), color='orange', alpha=0.2)
 
 
 
