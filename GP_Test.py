cf7feb1ca53605269c9624a84bf9481a99174db7
Björn Volkmann <bjoern.volkmann@imes.uni-hannover.de>
2024-01-31 10:48:29 +0100

    Made new implementation of the basis function expansion in the style of pytorch. Now a new model is created by writing a specific class and inheriting from the base class.

--- GP_Test.py
+++ GP_Test.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 
 
-from src.RGP import ApproximateGP, EnsambleGP, gaussian_RBF
+from src.RGP import BasisFunctionExpansion, gaussian_RBF
 
 
 
@@ -20,58 +20,43 @@ H = lambda x: gaussian_RBF(
     lengthscale=np.array([0.5])
 )
 
-model1 = ApproximateGP(
-    basis_function=H,
-    n_basis=len(inducing_points)
-)
-model1._cov *= 100
+class Model(BasisFunctionExpansion):
+    
+    def __init__(self) -> None:
+        super().__init__(inducing_points.size)
+        
+        self.register_basis_function(H)
+        self.initialize_prior(
+            np.zeros((inducing_points.size,)),
+            np.eye(inducing_points.size)*100
+            )
+        
+model = Model()
 
-
-model2 = EnsambleGP(
-    basis_function=H,
-    n_basis=len(inducing_points),
-    w0=np.zeros(inducing_points.size),
-    cov0=np.eye(inducing_points.size)*10**2,
-    N=50,
-    error_cov=0.5**2
-)
 
 
 rng = np.random.default_rng()
-for i in tqdm(range(1000)):
-    X_train = rng.uniform(-10,10,(20,1))
+for i in tqdm(range(10000)):
+    X_train = rng.uniform(-10,10,1)
     Y_train = test_function1(X_train) + rng.normal(0, 1, X_train.shape)
-    model1.update(X_train, Y_train.flatten(), np.ones((X_train.size,)))
-    model2.update(X_train, Y_train, np.ones((X_train.size,)))
+    model.fit_BOLS(X_train, Y_train, [1])
 
 
 
 ################################################################################
 
 
-fig1, ax1 = plt.subplots(3,1, layout='tight')
+fig1, ax1 = plt.subplots(1,1,layout='tight')
 
 
 Xnew = np.arange(-10,10,0.1)
 F = test_function1(Xnew)
-m_F, p_F = model1.predict(Xnew.reshape((Xnew.size,1)))
+m_F, p_F = model(Xnew.reshape((Xnew.size,1)))
 s = np.sqrt(np.diag(p_F))
 
-F_pred = model2.predict(Xnew.reshape((Xnew.size,1)))
-F_pred_mean = F_pred.mean(axis=1)
-
-ax1[0].plot(Xnew, F, label='True Function')
-ax1[0].plot(Xnew, m_F.flatten(), linestyle='--', color='orange', label='GP mean')
-ax1[0].fill_between(Xnew, m_F.flatten() - 3*s, m_F.flatten() + 3*s, color='orange', alpha=0.2)
-
-ax1[1].plot(Xnew, F, label='True Function')
-ax1[1].plot(Xnew, F_pred, color='orange', alpha=5/model2.W.shape[0])
-ax1[1].plot(Xnew, F_pred_mean.flatten(), linestyle='--', color='orange', label='GP mean')
-
-var = np.var(F_pred, axis=1)
-ax1[2].plot(Xnew, F, label='True Function')
-ax1[2].plot(Xnew, F_pred_mean.flatten(), linestyle='--', color='orange', label='GP mean')
-ax1[2].fill_between(Xnew, F_pred_mean - 3*np.sqrt(var), F_pred_mean + 3*np.sqrt(var), color='orange', alpha=0.2)
+ax1.plot(Xnew, F, label='True Function')
+ax1.plot(Xnew, m_F.flatten(), linestyle='--', color='orange', label='GP mean')
+ax1.fill_between(Xnew, m_F.flatten() - 3*s, m_F.flatten() + 3*s, color='orange', alpha=0.2)
 
 
 plt.show()
