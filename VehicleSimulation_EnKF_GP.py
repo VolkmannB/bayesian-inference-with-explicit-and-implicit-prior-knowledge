aa73eb07f46a197bca05cff262ea29e75c8f1f7f
Björn Volkmann <bjoern.volkmann@imes.uni-hannover.de>
2024-02-12 13:03:10 +0100

    starting algorithm implementation without objects where possible

--- VehicleSimulation_EnKF_GP.py
+++ VehicleSimulation_EnKF_GP.py
@@ -6,7 +6,6 @@ from tqdm import tqdm
 
 from src.vehicle.Vehicle import H_vehicle, vehicle_RBF_ip, default_para, f_x, f_y, fx_filter, fy_filter
 from src.RGP import EnsambleGP, sq_dist
-from src.KalmanFilter import EnsambleKalmanFilter
 
 
 
@@ -58,21 +57,14 @@ P_ = P0
 x_part = np.zeros((N,3))
 
 # noise
-meas_noise_var = np.diag([0.01/180*np.pi, 1e-3])
-process_var = np.diag([5e-6, 5e-7, 1e-3, 1e-3])
+R = np.diag([0.01/180*np.pi, 1e-3])
+Q = np.diag([5e-6, 5e-7, 1e-3, 1e-3])
+w = lambda size=...: np.random.multivariate_normal(np.zeros((Q.shape[0],)), Q, size)
+e = lambda size=...: np.random.multivariate_normal(np.zeros((R.shape[0],)), R, size)
 
 fx_EnKF = lambda x, u, theta_f, theta_r: fx_filter(x, u, theta_f, theta_r, dt, *default_para[1:7])
 fy_EnKF = lambda x, u: fy_filter(x, u, *default_para[1:7])
 
-EnKF = EnsambleKalmanFilter(
-    N,
-    x0,
-    P0,
-    jax.vmap(fx_EnKF),
-    jax.vmap(fy_EnKF),
-    process_var,
-    meas_noise_var
-)
 
 
 ################################################################################
@@ -93,21 +85,24 @@ CW = np.zeros((steps, vehicle_RBF_ip.shape[0], vehicle_RBF_ip.shape[0]))
 
 # input
 u = np.zeros((steps,2))
-u[:,1] = 10
+u[:,1] = 8.0
+u[int(len(time)/4):,0] = 20/180*np.pi
+u[int(len(time)/2):,0] = -20/180*np.pi
+u[int(3*len(time)/4):,0] = 0.0
 
 # simulation loop
 for i in tqdm(range(1,steps), desc="Running simulation"):
     
     ####### Model simulation
     X[i] = f_x(X[i-1], u[i-1], *default_para)
-    Y[i] = f_y_sim(X[i], u[i-1]) + np.random.multivariate_normal(np.zeros((2,)), meas_noise_var)
+    Y[i] = f_y_sim(X[i], u[i-1]) + np.random.multivariate_normal(np.zeros((2,)), R)
     
     
     
     ####### Filtering
     
     # time update
-    EnKF.predict(u=np.repeat(u[i-1], N), theta_f=mu_f_model.W, theta_r=mu_r_model.W)
+    
     
     # measurment update
-    EnKF.update(Y[i], u=np.repeat(u[i-1], N))
+    
