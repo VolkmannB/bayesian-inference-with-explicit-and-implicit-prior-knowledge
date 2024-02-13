fixed error in the calculation of the side slip angle

src/vehicle/Vehicle.py
@@ -42,7 +42,7 @@ def f_alpha(x, u, **para):
     vy_f = x[1] + x[0]*para['l_f']
     
     vx_r = u[1]
-    vy_r = x[1] - x[0]*para['l_f']
+    vy_r = x[1] - x[0]*para['l_r']
     
     return u[0]-jnp.arctan(vy_f/vx_f), -jnp.arctan(vy_r/vx_r)
 
@@ -237,7 +237,7 @@ def forward_Bootstrap_PF(Y, U, X_0, para, Q, R):
 ##### Filtering
 
 # features for front and rear tire
-vehicle_RBF_ip = jnp.atleast_2d(jnp.linspace(-20/180*jnp.pi, 20/180*jnp.pi, 21)).T
+vehicle_RBF_ip = jnp.atleast_2d(jnp.linspace(-20/180*jnp.pi, 20/180*jnp.pi, 20)).T
 vehicle_lengthscale = vehicle_RBF_ip[1] - vehicle_RBF_ip[0]
 H_vehicle = lambda alpha: gaussian_RBF(alpha, vehicle_RBF_ip, vehicle_lengthscale)
 
@@ -254,7 +254,7 @@ def features_MTF_front(x, u, **para):
 def features_MTF_rear(x, u, **para):
     
     vx = u[1]
-    vy = x[1] - x[0]*para['l_f']
+    vy = x[1] - x[0]*para['l_r']
     alpha = -jnp.arctan(vy/vx)
     
     return H_vehicle(alpha)