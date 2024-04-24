f60520e8e791cad98788d3c925b1dee0f66a74d1
Björn Volkmann <bjoern.volkmann@imes.uni-hannover.de>
2024-04-24 10:55:02 +0200

    Vehicle simulation without learning works now

--- VehicleSimulation_EnKF_GP.py
+++ VehicleSimulation_EnKF_GP.py
@@ -1,13 +1,17 @@
 import numpy as np
 import jax
+import jax.numpy as jnp
 from tqdm import tqdm
 import functools
 
 
 
-from src.vehicle.Vehicle import features_MTF_front, features_MTF_rear, vehicle_RBF_ip, default_para, f_x_sim, f_y, fx_filter, fy_filter, f_alpha, mu_y, H_vehicle
-from src.RGP import EnsambleGP, update_EIV_normal
-from src.KalmanFilter import EnKF_update
+from src.vehicle.Vehicle import features_MTF_front, features_MTF_rear
+from src.vehicle.Vehicle import vehicle_RBF_ip, default_para, f_x_sim, f_y
+from src.vehicle.Vehicle import fx_filter, fy_filter, f_alpha, mu_y, H_vehicle
+from src.RGP import prior_mniw_2naturalPara, prior_mniw_2naturalPara_inv
+from src.RGP import prior_mniw_updateStatistics, prior_mniw_sampleLikelihood
+from src.KalmanFilter import squared_error, systematic_SISR
 from src.vehicle.VehiclePlotting import generate_Vehicle_Animation
 
 
@@ -18,48 +22,59 @@ from src.vehicle.VehiclePlotting import generate_Vehicle_Animation
 rng = np.random.default_rng()
 
 # sim para
-N = 40
+N = 200
+N_ip = vehicle_RBF_ip.shape[0]
 t_end = 100.0
 time = np.arange(0.0, t_end, default_para['dt'])
 steps = len(time)
 
 
-# model prior
-prior = [
-    np.zeros((vehicle_RBF_ip.shape[0],)),
-    np.eye(vehicle_RBF_ip.shape[0])
+# model prior front tire
+phi = jax.vmap(H_vehicle)(vehicle_RBF_ip)
+GP_prior_f = list(prior_mniw_2naturalPara(
+    np.zeros((1, N_ip)),
+    np.eye(N_ip),
+    np.eye(1)*1e-3,
+    0
+))
+
+# model prior rear tire
+GP_prior_r = list(prior_mniw_2naturalPara(
+    np.zeros((1, N_ip)),
+    np.eye(N_ip),
+    np.eye(1)*1e-3,
+    0
+))
+
+# parameters for the sufficient statistics
+GP_stats_f = [
+    np.zeros((N, N_ip, 1)),
+    np.zeros((N, N_ip, N_ip)),
+    np.zeros((N, 1, 1)),
+    np.zeros((N,))
 ]
-noise = [
-    np.zeros((vehicle_RBF_ip.shape[0],)),
-    np.eye(vehicle_RBF_ip.shape[0])*1e-8
-]
-
-# model for the front tire
-para_model_f = [
-    prior[0],
-    prior[1]
-]
-
-# model for the rear tire
-para_model_r = [
-    prior[0],
-    prior[1]
+GP_stats_r = [
+    np.zeros((N, N_ip, 1)),
+    np.zeros((N, N_ip, N_ip)),
+    np.zeros((N, 1, 1)),
+    np.zeros((N,))
 ]
 
 
 
-# initial state
-x0 = np.array([0.0, 0.0, 0.0, 0.0])
-x_ = x0[0:2]
-P0 = np.diag([1e-4, 1e-4, 1e-4, 1e-4])
-P_ = P0
-x_part = np.zeros((N,3))
+# initial system state
+x0 = np.array([0.0, 0.0])
+P0 = np.diag([1e-4, 1e-4])
+np.random.seed(573573)
+key = jax.random.key(np.random.randint(100, 1000))
+print(f"Initial jax-key is: {key}")
 
 # noise
-R = np.diag([0.01/180*np.pi, 1e-4])
-Q = np.diag([5e-6, 5e-7, 1e-4, 1e-3])
-w = lambda size=(): np.random.multivariate_normal(np.zeros((Q.shape[0],)), Q, size)
-e = lambda size=(): np.random.multivariate_normal(np.zeros((R.shape[0],)), R, size)
+R = np.diag([0.01/180*np.pi, 1e-0])
+Q = np.diag([5e-4, 5e-4])
+R_y = 5e-0
+w = lambda n=1: np.random.multivariate_normal(np.zeros((Q.shape[0],)), Q, n)
+e = lambda n=1: np.random.multivariate_normal(np.zeros((R.shape[0],)), R, n)
 
 
 
@@ -70,7 +85,9 @@ e = lambda size=(): np.random.multivariate_normal(np.zeros((R.shape[0],)), R, si
 X = np.zeros((steps,2)) # sim
 Y = np.zeros((steps,2))
 
-Sigma_X = np.zeros((steps,N,4))
+Sigma_X = np.zeros((steps,N,2))
+Sigma_mu_f = np.zeros((steps,N))
+Sigma_mu_r = np.zeros((steps,N))
 
 W_f = np.zeros((steps, vehicle_RBF_ip.shape[0]))
 CW_f = np.zeros((steps, vehicle_RBF_ip.shape[0], vehicle_RBF_ip.shape[0]))
@@ -78,76 +95,214 @@ W_r = np.zeros((steps, vehicle_RBF_ip.shape[0]))
 CW_r = np.zeros((steps, vehicle_RBF_ip.shape[0], vehicle_RBF_ip.shape[0]))
 
 # input
-u = np.zeros((steps,2))
-u[:,0] = 10/180*np.pi * np.sin(2*np.pi*time/5) * 0.5*(np.tanh(0.2*(time-15))-np.tanh(0.2*(time-75)))
-u[:,1] = 11.0
+ctrl_input = np.zeros((steps,2))
+ctrl_input[:,0] = 10/180*np.pi * np.sin(2*np.pi*time/5) * 0.5*(np.tanh(0.2*(time-15))-np.tanh(0.2*(time-75)))
+ctrl_input[:,1] = 11.0
 
 # initial values
 Sigma_X[0,...] = np.random.multivariate_normal(x0, P0, (N,))
-W_f[0,:] = para_model_f[0]
-CW_f[0,:] = para_model_f[1]
-W_r[0,:] = para_model_r[0]
-CW_r[0,:] = para_model_r[1]
+X[0,...] = x0
+weights = np.ones((steps,N))/N
 
-# initial training
+# initial value for GP
+phi_f = jax.vmap(
+    functools.partial(features_MTF_front, u=ctrl_input[0], **default_para)
+    )(Sigma_X[0])
+phi_r = jax.vmap(
+    functools.partial(features_MTF_rear, u=ctrl_input[0], **default_para)
+    )(Sigma_X[0])
+GP_stats_f = list(jax.vmap(prior_mniw_updateStatistics)(
+    *GP_stats_f,
+    np.zeros((N,)),
+    phi_f
+))
+GP_stats_r = list(jax.vmap(prior_mniw_updateStatistics)(
+    *GP_stats_r,
+    np.zeros((N,)),
+    phi_r
+))
 
 
 # simulation loop
 for i in tqdm(range(1,steps), desc="Running simulation"):
     
     ####### Model simulation
-    X[i] = f_x_sim(X[i-1], u[i-1], **default_para)
-    Y[i] = f_y(X[i], u[i], **default_para) + e()
+    X[i] = f_x_sim(X[i-1], ctrl_input[i-1], **default_para)
+    Y[i] = f_y(X[i], ctrl_input[i], **default_para) + e()
     
     
     
     ####### Filtering
     
     # time update
-    Sigma_X[i,...] = jax.vmap(functools.partial(fx_filter, u=u[i-1], **default_para))(
-        x = Sigma_X[i-1,...],
-        theta_f=para_model_f[0] + (np.linalg.cholesky(para_model_f[1]) @ np.random.randn(vehicle_RBF_ip.shape[0],N)).T, 
-        theta_r=para_model_r[0] + (np.linalg.cholesky(para_model_r[1]) @ np.random.randn(vehicle_RBF_ip.shape[0],N)).T
-    ) + w((N,))
     
-    # measurment update
-    sigma_y = jax.vmap(functools.partial(fy_filter, u=u[i], **default_para))(
-        x = Sigma_X[i,...]
+    # mu
+    alpha_f, alpha_r = jax.vmap(
+        functools.partial(f_alpha, u=ctrl_input[i-1], **default_para)
+        )(x=Sigma_X[i-1])
+    mu_f = mu_y(alpha_f, default_para['mu'], default_para['B_f'], default_para['C_f'], default_para['E_f'])
+    mu_r = mu_y(alpha_r, default_para['mu'], default_para['B_r'], default_para['C_r'], default_para['E_r'])
+    
+    ### Step 1: Evaluate basis functions and calculate standard parameters 
+    # of the GP
+    
+    # evaluate basis functions
+    phi_f = jax.vmap(
+        functools.partial(features_MTF_front, u=ctrl_input[i], **default_para)
+        )(Sigma_X[i-1])
+    phi_r = jax.vmap(
+        functools.partial(features_MTF_rear, u=ctrl_input[i], **default_para)
+        )(Sigma_X[i-1])
+        
+    # calculate parameters of GP from prior and sufficient statistics
+    GP_para_f = list(jax.vmap(prior_mniw_2naturalPara_inv)(
+        GP_prior_f[0] + GP_stats_f[0],
+        GP_prior_f[1] + GP_stats_f[1],
+        GP_prior_f[2] + GP_stats_f[2],
+        GP_prior_f[3] + GP_stats_f[3]
+    ))
+    GP_para_r = list(jax.vmap(prior_mniw_2naturalPara_inv)(
+        GP_prior_r[0] + GP_stats_r[0],
+        GP_prior_r[1] + GP_stats_r[1],
+        GP_prior_r[2] + GP_stats_r[2],
+        GP_prior_r[3] + GP_stats_r[3]
+    ))
+        
+        
+        
+    ### Step 2: According to the algorithm of the auxiliary PF, resample 
+    # particles according to the first stage weights
+    
+    # create auxiliary variable
+    mu_f_aux = jax.vmap(jnp.matmul)(GP_para_f[0], phi_f).flatten()
+    mu_r_aux = jax.vmap(jnp.matmul)(GP_para_r[0], phi_r).flatten()
+    x_aux = jax.vmap(functools.partial(fx_filter, u=ctrl_input[i-1], **default_para))(
+        x=Sigma_X[i-1,...],
+        mu_yf=mu_f, 
+        mu_yr=mu_r
     )
-    Sigma_X[i,...] = EnKF_update(Sigma_X[i,...], sigma_y, np.concatenate([Y[i], [0]]), np.diag(np.concatenate([np.diag(R), [0.01]])))
     
-    # update model front
-    mu_x = np.mean(Sigma_X[i,...], axis=0)
-    P_x = np.cov(Sigma_X[i,...].T)
-    J_psi = jax.jacfwd(functools.partial(features_MTF_front, u=u[i], **default_para))(mu_x[:2])
-    psi = features_MTF_front(x=mu_x[:2], u=u[i], **default_para)
-    para_model_f = update_EIV_normal(
-        psi,
-        mu_x[2],
-        P_x[2,2] - 2*para_model_f[0]@J_psi@P_x[:2,2] + para_model_f[0]@J_psi@P_x[:2,:2]@J_psi.T@para_model_f[0].T,
-        *para_model_f
-    )
+    # calculate first stage weights
+    y_aux = jax.vmap(
+        functools.partial(fy_filter, u=ctrl_input[i], **default_para)
+        )(
+            x=x_aux,
+            mu_yf=mu_f, 
+            mu_yr=mu_r)
+    l = jax.vmap(functools.partial(squared_error, y=np.hstack((Y[i],0)), cov=np.diag(np.hstack((np.diag(R),R_y)))))(x=y_aux)
+    p = weights[i-1] * l
+    p = p/np.sum(p)
     
-    # update model back
-    J_psi = jax.jacfwd(functools.partial(features_MTF_rear, u=u[i], **default_para))(mu_x[:2])
-    psi = features_MTF_rear(x=mu_x[:2], u=u[i], **default_para)
-    para_model_r = update_EIV_normal(
-        psi,
-        mu_x[3],
-        P_x[3,3] - 2*psi@J_psi@P_x[:2,3] + psi@J_psi@P_x[:2,:2]@J_psi.T@psi.T,
-        *para_model_r
-    )
+    #abort
+    if np.any(np.isnan(p)):
+        print("Particle degeneration at auxiliary weights")
+        break
     
-    para_model_f[1] += noise[1]
-    para_model_r[1] += noise[1]
+    # draw new indices
+    u = np.random.rand()
+    idx = np.array(systematic_SISR(u, p))
+    idx[idx >= N] = N - 1 # correct out of bounds indices from numerical errors
+    
+    # copy statistics
+    GP_stats_f[0] = GP_stats_f[0][idx,...]
+    GP_stats_f[1] = GP_stats_f[1][idx,...]
+    GP_stats_f[2] = GP_stats_f[2][idx,...]
+    GP_stats_f[3] = GP_stats_f[3][idx,...]
+    GP_para_f[0] = GP_para_f[0][idx,...]
+    GP_para_f[1] = GP_para_f[1][idx,...]
+    GP_para_f[2] = GP_para_f[2][idx,...]
+    GP_para_f[3] = GP_para_f[3][idx,...]
+    phi_f = phi_f[idx]
+    GP_stats_r[0] = GP_stats_r[0][idx,...]
+    GP_stats_r[1] = GP_stats_r[1][idx,...]
+    GP_stats_r[2] = GP_stats_r[2][idx,...]
+    GP_stats_r[3] = GP_stats_r[3][idx,...]
+    GP_para_r[0] = GP_para_r[0][idx,...]
+    GP_para_r[1] = GP_para_r[1][idx,...]
+    GP_para_r[2] = GP_para_r[2][idx,...]
+    GP_para_r[3] = GP_para_r[3][idx,...]
+    phi_r = phi_r[idx]
+    Sigma_X[i-1] = Sigma_X[i-1,idx,...]
+    mu_r = mu_r[idx]
+    mu_f = mu_f[idx]
+    
+    # sample from proposal for mu
+    # key, *keys = jax.random.split(key, N+1)
+    # Sigma_mu_f[i-1] = jax.vmap(prior_mniw_sampleLikelihood)(
+    #     key=jnp.asarray(keys),
+    #     M=GP_para_f[0],
+    #     V=GP_para_f[1],
+    #     Psi=GP_para_f[2],
+    #     nu=GP_para_f[3],
+    #     phi=phi_f
+    # ).flatten()
+    Sigma_mu_f[i-1] = mu_f
+    
+    # key, *keys = jax.random.split(key, N+1)
+    # Sigma_mu_r[i-1] = jax.vmap(prior_mniw_sampleLikelihood)(
+    #     key=jnp.asarray(keys),
+    #     M=GP_para_r[0],
+    #     V=GP_para_r[1],
+    #     Psi=GP_para_r[2],
+    #     nu=GP_para_r[3],
+    #     phi=phi_r
+    # ).flatten()
+    Sigma_mu_r[i-1] = mu_r
+    
+    # sample from proposal for x
+    w_x = w((N,))
+    Sigma_X[i] = jax.vmap(
+        functools.partial(fx_filter, u=ctrl_input[i-1], **default_para)
+        )(
+            x=Sigma_X[i-1,:,...],
+            mu_yf=Sigma_mu_f[i-1],
+            mu_yr=Sigma_mu_r[i-1]
+            ) + w_x
+    
+    # apply forgetting operator to statistics for t+1
+    GP_stats_f[0] *= 0.999
+    GP_stats_f[1] *= 0.999
+    GP_stats_f[2] *= 0.999
+    GP_stats_f[3] *= 0.999
+    GP_stats_r[0] *= 0.999
+    GP_stats_r[1] *= 0.999
+    GP_stats_r[2] *= 0.999
+    GP_stats_r[3] *= 0.999
+    
+    # update GP parameters
+    GP_stats_f = list(jax.vmap(prior_mniw_updateStatistics)(
+        *GP_stats_f,
+        Sigma_mu_f[i-1],
+        phi_f
+    ))
+    
+    GP_stats_r = list(jax.vmap(prior_mniw_updateStatistics)(
+        *GP_stats_r,
+        Sigma_mu_r[i-1],
+        phi_r
+    ))
+    
+    # calculate new weights (measurment update)
+    sigma_y = jax.vmap(
+        functools.partial(fy_filter, u=ctrl_input[i], **default_para)
+        )(
+            x=Sigma_X[i],
+            mu_yf=Sigma_mu_f[i-1], 
+            mu_yr=Sigma_mu_r[i-1])
+    q = jax.vmap(functools.partial(squared_error, y=np.hstack((Y[i],0)), cov=np.diag(np.hstack((np.diag(R),R_y)))))(sigma_y)
+    weights[i] = q / p[idx]
+    weights[i] = weights[i]/np.sum(weights[i])
     
     # logging
-    W_f[i,:] = para_model_f[0]
-    CW_f[i,:] = para_model_f[1]
-    W_r[i,:] = para_model_r[0]
-    CW_r[i,:] = para_model_r[1]
+    W_f[i,...] = np.sum(weights[i,:,None,None] * GP_para_f[0], axis=0).flatten()
+    W_r[i,...] = np.sum(weights[i,:,None,None] * GP_para_r[0], axis=0).flatten()
+    
+    #abort
+    if np.any(np.isnan(weights[i])):
+        print("Particle degeneration at new weights")
+        break
     
     
 
-fig = generate_Vehicle_Animation(X, Y, u, Sigma_X, W_f, CW_f, W_r, CW_r, time, default_para, 200., 30., 30)
+fig = generate_Vehicle_Animation(X, Y, ctrl_input, weights, Sigma_X, Sigma_mu_f, Sigma_mu_r, W_f, CW_f, W_r, CW_r, time, default_para, 200., 30., 30)
 fig.show()
