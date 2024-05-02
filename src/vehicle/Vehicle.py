c36ca046de0e89db174b86bce76cfe1b10475c70
Björn Volkmann <bjoern.volkmann@imes.uni-hannover.de>
2024-05-02 14:43:18 +0200

    changed evaluation of equations to be in line with newly formulated theory

--- src/vehicle/Vehicle.py
+++ src/vehicle/Vehicle.py
@@ -122,10 +122,10 @@ def f_y(x, u, **para):
 
 # features for front and rear tire
 N_ip = 8
-vehicle_RBF_ip = jnp.atleast_2d(jnp.linspace(-20/180*jnp.pi, 20/180*jnp.pi, 8)).T
+vehicle_RBF_ip = jnp.atleast_2d(jnp.linspace(-20/180*jnp.pi, 20/180*jnp.pi, N_ip)).T
 vehicle_lengthscale = vehicle_RBF_ip[1] - vehicle_RBF_ip[0]
-# H_vehicle = lambda alpha: bump_RBF(alpha, vehicle_RBF_ip, vehicle_lengthscale)
-H_vehicle, sd = generate_Hilbert_BasisFunction(N_ip, np.array([-0.5, 0.5]), 1/N_ip, 1, j_start=2, j_step=2)
+H_vehicle = lambda alpha: bump_RBF(alpha, vehicle_RBF_ip, vehicle_lengthscale)
+# H_vehicle, sd = generate_Hilbert_BasisFunction(N_ip, np.array([-0.5, 0.5]), 1/N_ip, 1, j_start=2, j_step=2)
 
 @jax.jit
 def features_MTF_front(x, u, **para):
