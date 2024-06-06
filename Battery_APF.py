1574af7b68ff97e42b6b0f7b9acd98537168f53b
Björn Volkmann <bjoern.volkmann@imes.uni-hannover.de>
2024-06-06 11:42:11 +0200

    removed cell temperature from SSM. Measurement data does not seem to allow meaningful estimation and range is to small to have substantial effects. Learning of serial resistance R_0 is now redundant No more animations. It simply takes to long to render or save the file

--- Battery_APF.py
+++ Battery_APF.py
@@ -12,7 +12,7 @@ from plotly.subplots import make_subplots
 
 
 from src.Battery import BatterySSM, basis_fcn, default_para, z_ip
-from src.BayesianInferrence import prior_mniw_2naturalPara, prior_mniw_2naturalPara_inv
+from src.BayesianInferrence import prior_mniw_2naturalPara, prior_mniw_2naturalPara_inv, prior_mniw_Predictive
 from src.BayesianInferrence import prior_mniw_updateStatistics, prior_mniw_CondPredictive
 from src.Filtering import squared_error, systematic_SISR
 
@@ -33,36 +33,19 @@ y1_var=5e-2
 # parameter limits
 l_C1, u_C1 = 2, 20
 l_R1, u_R1 = 5, 10
-l_R0, u_R0 = 10e-3, 60e-3
 
 # model prior C1, R1
 N_z = z_ip.shape[0]
 GP_prior_C1R1 = list(prior_mniw_2naturalPara(
     np.zeros((2, N_z)),
-    np.eye(N_z)*20,
-    np.diag([1e-1, 1e-1]),
+    np.eye(N_z)*10,
+    np.diag([1, 1]),
     0
 ))
 GP_stats_C1R1 = [
     np.zeros((N, N_z, 2)),
     np.zeros((N, N_z, N_z)),
     np.zeros((N, 2, 2)),
-    np.zeros((N,))
-]
-
-# model prior R0
-GP_prior_R0 = list(prior_mniw_2naturalPara(
-    np.zeros((1, N_z)),
-    np.eye(N_z),
-    np.eye(1)*2e-3,
-    0
-))
-
-# parameters for the sufficient statistics
-GP_stats_R0 = [
-    np.zeros((N, N_z, 1)),
-    np.zeros((N, N_z, N_z)),
-    np.zeros((N, 1, 1)),
     np.zeros((N,))
 ]
 
@@ -79,9 +62,6 @@ data["Block"] = (data.index.to_series().diff() > time_gap_thresh).cumsum()
 blocks = [group for _, group in data.groupby("Block")]
 data = blocks[0]
 del blocks
-# start=358
-# end=10000
-# data = data.iloc[start:end]
 
 # resampling to uniform time steps
 dt = pd.Timedelta("1s")
@@ -91,19 +71,18 @@ data = data.iloc[:20000]
 steps = data.shape[0]
 
 # init model
-default_para["T_amb"] = data["Temperature"].iloc[0]
 model = BatterySSM(**default_para)
 
 
 
 # initial system state
-x0 = np.array([data["Voltage"].iloc[0]-default_para["V_0"], data["Temperature"].iloc[0]])
-P0 = np.diag([5e-3, 0.25])
+x0 = np.array([data["Voltage"].iloc[0]-default_para["V_0"]])
+P0 = np.diag([5e-3])
 # np.random.seed(573573)
 
 # process and measurement noise
-R = np.diag([0.001, 0.25])
-Q = np.diag([5e-6, 1e-3])
+R = np.diag([0.001])
+Q = np.diag([5e-6])
 w = lambda n=1: np.random.multivariate_normal(np.zeros((Q.shape[0],)), Q, n)
 e = lambda n=1: np.random.multivariate_normal(np.zeros((R.shape[0],)), R, n)
 
@@ -113,29 +92,29 @@ e = lambda n=1: np.random.multivariate_normal(np.zeros((R.shape[0],)), R, n)
 # Simulation
 
 # time series for plot
-
-Sigma_X = np.zeros((steps,N,2))
-Sigma_Y = np.zeros((steps,N,2))
+Sigma_X = np.zeros((steps,N))
+Sigma_Y = np.zeros((steps,N))
 Sigma_C1R1 = np.zeros((steps,N,2))
-Sigma_R0 = np.zeros((steps,N))
 
-A_C1R1 = np.zeros((steps, 2, N_z))
-A_R0 = np.zeros((steps, 1, N_z))
+Mean_C1R1 = np.zeros((steps, 2, N_z))
+Col_Cov_C1R1 = np.zeros((steps, N_z, N_z))
+Row_Scale_C1R1 = np.zeros((steps, 2, 2))
+df_C1R1 = np.zeros((steps,))
 
 # input
 ctrl_input = data["Current"].to_numpy()
 
 # measurments
-Y = data[["Voltage", "Temperature"]].to_numpy()
+Y = data[["Voltage"]].to_numpy()
+
 
 
 ### Set all initial values
 
 # initial values for states
-Sigma_X[0,...] = np.random.multivariate_normal(x0, P0, (N,))
+Sigma_X[0,...] = np.random.multivariate_normal(x0, P0, (N,)).flatten()
 Sigma_C1R1[0,:,0] = np.random.uniform(l_C1, u_C1, (N,))
 Sigma_C1R1[0,:,1] = np.random.uniform(l_R1, u_R1, (N,))
-Sigma_R0[0,...] = np.random.uniform(l_R0, u_R0, (N,))
 weights = np.ones((steps,N))/N
 
 # update GP
@@ -147,11 +126,6 @@ GP_stats_C1R1 = list(jax.vmap(prior_mniw_updateStatistics)(
     Sigma_C1R1[0,...],
     phi_0
 ))
-# GP_stats_R0 = list(jax.vmap(prior_mniw_updateStatistics)(
-#     *GP_stats_R0,
-#     Sigma_R0[0,...],
-#     phi_0
-# ))
 
 
 # simulation loop
@@ -161,14 +135,10 @@ for i in tqdm(range(1, steps), desc="Running simulation"):
     dt = (data.index[i] - data.index[i-1]).seconds
     
     # apply forgetting operator to statistics for t-1 -> t
-    GP_stats_C1R1[0] *= 1 - 1/2e3
-    GP_stats_C1R1[1] *= 1 - 1/2e3
-    GP_stats_C1R1[2] *= 1 - 1/2e3
-    GP_stats_C1R1[3] *= 1 - 1/2e3
-    # GP_stats_R0[0] *= 0.999
-    # GP_stats_R0[1] *= 0.999
-    # GP_stats_R0[2] *= 0.999
-    # GP_stats_R0[3] *= 0.999
+    GP_stats_C1R1[0] *= 1 - 1/1e3
+    GP_stats_C1R1[1] *= 1 - 1/1e3
+    GP_stats_C1R1[2] *= 1 - 1/1e3
+    GP_stats_C1R1[3] *= 1 - 1/1e3
         
     # calculate parameters of GP from prior and sufficient statistics
     GP_para_C1R1 = list(jax.vmap(prior_mniw_2naturalPara_inv)(
@@ -177,12 +147,6 @@ for i in tqdm(range(1, steps), desc="Running simulation"):
         GP_prior_C1R1[2] + GP_stats_C1R1[2],
         GP_prior_C1R1[3] + GP_stats_C1R1[3]
     ))
-    # GP_para_R0 = list(jax.vmap(prior_mniw_2naturalPara_inv)(
-    #     GP_prior_R0[0] + GP_stats_R0[0],
-    #     GP_prior_R0[1] + GP_stats_R0[1],
-    #     GP_prior_R0[2] + GP_stats_R0[2],
-    #     GP_prior_R0[3] + GP_stats_R0[3]
-    # ))
     
     
     
@@ -191,7 +155,7 @@ for i in tqdm(range(1, steps), desc="Running simulation"):
     
     # create auxiliary variable for state x
     x_aux = jax.vmap(
-        functools.partial(model, I=ctrl_input[i-1], dt=dt, R_0=23e-3))(
+        functools.partial(model, I=ctrl_input[i-1], dt=dt))(
             x=Sigma_X[i-1],
             C_1=Sigma_C1R1[i-1,...,0]*1e2,
             R_1=Sigma_C1R1[i-1,...,1]*1e3
@@ -211,27 +175,14 @@ for i in tqdm(range(1, steps), desc="Running simulation"):
             basis1=phi_0,
             basis2=phi_1
     )[0]
-    # R0_aux = jax.vmap(
-    #     functools.partial(prior_mniw_CondPredictive, y1_var=y1_var)
-    #     )(
-    #         mean=GP_para_R0[0],
-    #         col_cov=GP_para_R0[1],
-    #         row_scale=GP_para_R0[2],
-    #         df=GP_para_R0[3],
-    #         y1=Sigma_R0[i-1,...],
-    #         basis1=phi_0,
-    #         basis2=phi_1
-    # )[0]
     
     # calculate first stage weights
     y_aux = jax.vmap(
         functools.partial(model.fy, I=ctrl_input[i], R_0=23e-3)
-        )(
-            x=x_aux)
+        )(x=x_aux)
     l_fy = jax.vmap(functools.partial(squared_error, y=Y[i], cov=R))(x=y_aux)
     l_C0_clip = np.all(np.vstack([l_C1 <= C1R1_aux[:,0], C1R1_aux[:,0] <= u_C1]), axis=0)
     l_R0_clip = np.all(np.vstack([l_R1 <= C1R1_aux[:,1], C1R1_aux[:,1] <= u_R1]), axis=0)
-    # l_R1_clip = np.all(np.vstack([l_R0 <= R0_aux, R0_aux <= u_R0]), axis=0)
     p = weights[i-1] * l_fy * l_C0_clip * l_R0_clip
     p = p/np.sum(p)
     
@@ -255,18 +206,8 @@ for i in tqdm(range(1, steps), desc="Running simulation"):
     GP_para_C1R1[2] = GP_para_C1R1[2][idx,...]
     GP_para_C1R1[3] = GP_para_C1R1[3][idx,...]
     
-    # GP_stats_R0[0] = GP_stats_R0[0][idx,...]
-    # GP_stats_R0[1] = GP_stats_R0[1][idx,...]
-    # GP_stats_R0[2] = GP_stats_R0[2][idx,...]
-    # GP_stats_R0[3] = GP_stats_R0[3][idx,...]
-    # GP_para_R0[0] = GP_para_R0[0][idx,...]
-    # GP_para_R0[1] = GP_para_R0[1][idx,...]
-    # GP_para_R0[2] = GP_para_R0[2][idx,...]
-    # GP_para_R0[3] = GP_para_R0[3][idx,...]
-    
     Sigma_X[i-1] = Sigma_X[i-1,idx,...]
     Sigma_C1R1[i-1] = Sigma_C1R1[i-1,idx,...]
-    # Sigma_R0[i-1] = Sigma_R0[i-1,idx]
     
     
     
@@ -274,9 +215,9 @@ for i in tqdm(range(1, steps), desc="Running simulation"):
     # model
     
     # sample from proposal for x at time t
-    w_x = w((N,))
+    w_x = w((N,)).flatten()
     Sigma_X[i] = jax.vmap(
-        functools.partial(model, I=ctrl_input[i-1], dt=dt, R_0=23e-3))(
+        functools.partial(model, I=ctrl_input[i-1], dt=dt))(
             x=Sigma_X[i-1],
             C_1=Sigma_C1R1[i-1,:,0]*1e2,
             R_1=Sigma_C1R1[i-1,:,1]*1e3
@@ -310,27 +251,6 @@ for i in tqdm(range(1, steps), desc="Running simulation"):
             t_samples, 
             c_col_scale_chol
         ))
-    
-    
-    
-    # # calculate conditional predictive distribution for R0
-    # c_mean, c_col_scale, c_row_scale, df = jax.vmap(
-    #     functools.partial(prior_mniw_CondPredictive, y1_var=y1_var)
-    #     )(
-    #         mean=GP_para_R0[0],
-    #         col_cov=GP_para_R0[1],
-    #         row_scale=GP_para_R0[2],
-    #         df=GP_para_R0[3],
-    #         y1=Sigma_R0[i-1,...],
-    #         basis1=phi_0,
-    #         basis2=phi_1
-    # )
-    
-    # # generate samples
-    # c_col_scale_chol = np.sqrt(np.squeeze(c_col_scale))
-    # c_row_scale_chol = np.sqrt(np.squeeze(c_row_scale))
-    # t_samples = np.random.standard_t(df=df)
-    # Sigma_R0[i] = c_mean + c_col_scale_chol * t_samples * c_row_scale_chol
         
         
     
@@ -340,21 +260,14 @@ for i in tqdm(range(1, steps), desc="Running simulation"):
         Sigma_C1R1[i],
         phi_1
     ))
-    # GP_stats_R0 = list(jax.vmap(prior_mniw_updateStatistics)(
-    #     *GP_stats_R0,
-    #     Sigma_R0[i],
-    #     phi_1
-    # ))
     
     # calculate new weights
     Sigma_Y[i] = jax.vmap(
         functools.partial(model.fy, I=ctrl_input[i], R_0=23e-3)
-        )(
-            x=Sigma_X[i])
+        )(x=Sigma_X[i])
     q_fy = jax.vmap(functools.partial(squared_error, y=Y[i], cov=R))(Sigma_Y[i])
     q_C0_clip = np.all(np.vstack([l_C1 <= Sigma_C1R1[i,:,0], Sigma_C1R1[i,:,0] <= u_C1]), axis=0)
     q_R0_clip = np.all(np.vstack([l_R1 <= Sigma_C1R1[i,:,1], Sigma_C1R1[i,:,1] <= u_R1]), axis=0)
-    # q_R1_clip = np.all(np.vstack([l_R0 <= Sigma_R0[i], Sigma_R0[i] <= u_R0]), axis=0)
     weights[i] = q_fy * q_C0_clip * q_R0_clip / p[idx]
     weights[i] = weights[i]/np.sum(weights[i])
     
@@ -367,15 +280,10 @@ for i in tqdm(range(1, steps), desc="Running simulation"):
         GP_prior_C1R1[2] + np.einsum('n...,n->...', GP_stats_C1R1[2], weights[i]),
         GP_prior_C1R1[3] + np.einsum('n...,n->...', GP_stats_C1R1[3], weights[i])
     )
-    A_C1R1[i] = GP_para_logging_C1R1[0]
-    
-    # GP_para_logging_R0 = prior_mniw_2naturalPara_inv(
-    #     GP_prior_R0[0] + np.einsum('n...,n->...', GP_stats_R0[0], weights[i]),
-    #     GP_prior_R0[1] + np.einsum('n...,n->...', GP_stats_R0[1], weights[i]),
-    #     GP_prior_R0[2] + np.einsum('n...,n->...', GP_stats_R0[2], weights[i]),
-    #     GP_prior_R0[3] + np.einsum('n...,n->...', GP_stats_R0[3], weights[i])
-    # )
-    # A_R0[i] = GP_para_logging_R0[0]
+    Mean_C1R1[i] = GP_para_logging_C1R1[0]
+    Col_Cov_C1R1[i] = GP_para_logging_C1R1[1]
+    Row_Scale_C1R1[i] = GP_para_logging_C1R1[2]
+    df_C1R1[i] = GP_para_logging_C1R1[3]
     
     #abort
     if np.any(np.isnan(weights[i])):
@@ -387,158 +295,51 @@ for i in tqdm(range(1, steps), desc="Running simulation"):
 ################################################################################
 # Plotting
 
-X_pred = np.einsum('...ni,...n->...i', Sigma_X, weights)
-Y_pred = np.einsum('...ni,...n->...i', Sigma_Y, weights)
-### give a plot of the data
-# data_fig = make_subplots(3,1)
-# data_fig.add_trace(
-#     go.Scatter(
-#         x=data.index,
-#         y=data["Voltage"],
-#         mode='markers',
-#         marker=dict(
-#             color='blue',
-#             size=4
-#         )
-#     ),
-#     row=1,
-#     col=1
-# )
-# data_fig.add_trace(
-#     go.Scatter(
-#         x=data.index,
-#         y=data["Current"],
-#         mode='markers',
-#         marker=dict(
-#             color='blue',
-#             size=4
-#         )
-#     ),
-#     row=2,
-#     col=1
-# )
-# data_fig.add_trace(
-#     go.Scatter(
-#         x=data.index,
-#         y=data["Temperature"],
-#         mode='markers',
-#         marker=dict(
-#             color='blue',
-#             size=4
-#         )
-#     ),
-#     row=3,
-#     col=1
-# )
-
-
-# # plot predicted observations
-# data_fig.add_trace(
-#     go.Scatter(
-#         x=data.index[1:],
-#         y=Y_pred[1:,0],
-#         mode='lines',
-#         line=dict(
-#             color='orange',
-#             width=2
-#         )
-#     ),
-#     row=1,
-#     col=1
-# )
-# data_fig.add_trace(
-#     go.Scatter(
-#         x=data.index[1:],
-#         y=Y_pred[1:,1],
-#         mode='lines',
-#         line=dict(
-#             color='orange',
-#             width=2
-#         )
-#     ),
-#     row=3,
-#     col=1
-# )
-# data_fig.update_layout(showlegend=False)
-# data_fig.update_yaxes(title_text='Voltage in [V]', row=1, col=1)
-# data_fig.update_yaxes(title_text='Curren in [A]', row=2, col=1)
-# data_fig.update_yaxes(title_text='Temperature in [C]', row=3, col=1)
-# data_fig.update_xaxes(title_text='Time', row=3, col=1)
-# data_fig.update_layout(font_size=24)
-# data_fig.show()
+X_pred = np.einsum('...n,...n->...', Sigma_X, weights)
+Y_pred = np.einsum('...n,...n->...', Sigma_Y, weights)
 
 
 
-fig_data, ax_data = plt.subplots(3,1, layout='tight')
+# plot data
+fig_data, ax_data = plt.subplots(2,1, layout='tight')
 
 ax_data[0].plot(data.index, data["Voltage"], color='blue', label='Meas')
-ax_data[0].plot(data.index[1:], Y_pred[1:,0], color='orange', label='Pred')
+ax_data[0].plot(data.index[1:], Y_pred[1:], color='orange', label='Pred')
 
 ax_data[1].plot(data.index, data["Current"], color='blue', label='Meas')
 
-ax_data[2].plot(data.index, data["Temperature"], color='blue', label='Meas')
-ax_data[2].plot(data.index[1:], Y_pred[1:,1], color='orange', label='Pred')
 
 
-# create animation
-fig_C1R1, ax_C1R1 = plt.subplots(2,1, layout='tight', dpi=200)
+# plot learned function
+fig_C1R1, ax_C1R1 = plt.subplots(2,6, layout='tight')
 V = jnp.linspace(0, 2.4, 500)
 phi = jax.vmap(basis_fcn)(V[...,None])
 
-# initial plot
-tmp = A_C1R1[0] @ phi.T
-line_C1, = ax_C1R1[0].plot(V, tmp[0,:])
-line_R1, = ax_C1R1[1].plot(V, tmp[1,:])
-scatter_C1 = ax_C1R1[0].scatter(Sigma_X[0,:,0], Sigma_C1R1[0,:,0], color='red', s=3)
-scatter_R1 = ax_C1R1[1].scatter(Sigma_X[0,:,0], Sigma_C1R1[0,:,1], color='red', s=3)
-
 # label
-ax_C1R1[0].set_ylabel('Capacity in F x1e2')
-ax_C1R1[1].set_ylabel('Resistance in Ohm x1e3')
+ax_C1R1[0,0].set_ylabel('Capacity in F x1e2')
+ax_C1R1[1,0].set_ylabel('Resistance in Ohm x1e3')
 
-# set limits
-ax_C1R1[0].set_ylim(np.min(Sigma_C1R1[...,0])-1, np.max(Sigma_C1R1[...,0])+1)
-ax_C1R1[1].set_ylim(np.min(Sigma_C1R1[...,1])-1, np.max(Sigma_C1R1[...,1])+1)
-
-
-def animate(i):
+# function
+for i in range(6):
+    ax_C1R1[0,i].set_ylim(0, 20)
+    ax_C1R1[1,i].set_ylim(0, 15)
     
-    tmp = A_C1R1[i] @ phi.T
-    line_C1.set_ydata(tmp[0,:])
-    line_R1.set_ydata(tmp[1,:])
+    mean, col_scale, row_scale, df = prior_mniw_Predictive(
+        mean=Mean_C1R1[int((i+1)/6*steps)-1], 
+        col_cov=Col_Cov_C1R1[int((i+1)/6*steps)-1], 
+        row_scale=Row_Scale_C1R1[int((i+1)/6*steps)-1], 
+        df=df_C1R1[int((i+1)/6*steps)-1], 
+        basis=phi
+        )
+    std0 = np.diag(np.linalg.cholesky(col_scale*row_scale[0,0]))
+    std1 = np.diag(np.linalg.cholesky(col_scale*row_scale[1,1]))
     
-    scatter_C1.set_offsets(np.vstack([Sigma_X[i,:,0], Sigma_C1R1[i,:,0]]).T)
-    scatter_R1.set_offsets(np.vstack([Sigma_X[i,:,0], Sigma_C1R1[i,:,1]]).T)
+    ax_C1R1[0,i].plot(V, mean[:,0], color='blue')
+    ax_C1R1[0,i].fill_between(V, mean[:,0]-std0, mean[:,0]+std0, color='blue', alpha=0.2)
     
-    return line_C1, line_R1, scatter_C1, scatter_R1
+    ax_C1R1[1,i].plot(V, mean[:,1], color='blue')
+    ax_C1R1[1,i].fill_between(V, mean[:,1]-std1, mean[:,1]+std1, color='blue', alpha=0.2)
 
-ani = animation.FuncAnimation(fig_C1R1, animate, frames=steps, blit=True, interval=1)
-# ani.to_jshtml()
-# ani.save("animation.mp4")
-
-# tmp = A_C1R1[int(1/6*steps)-1] @ phi.T
-# ax_C1R1[0,0].plot(V, tmp[0,:])
-# ax_C1R1[0,1].plot(V, tmp[1,:])
-
-# tmp = A_C1R1[int(2/6*steps)-1] @ phi.T
-# ax_C1R1[1,0].plot(V, tmp[0,:])
-# ax_C1R1[1,1].plot(V, tmp[1,:])
-
-# tmp = A_C1R1[int(3/6*steps)-1] @ phi.T
-# ax_C1R1[2,0].plot(V, tmp[0,:])
-# ax_C1R1[2,1].plot(V, tmp[1,:])
-
-# tmp = A_C1R1[int(4/6*steps)-1] @ phi.T
-# ax_C1R1[3,0].plot(V, tmp[0,:])
-# ax_C1R1[3,1].plot(V, tmp[1,:])
-
-# tmp = A_C1R1[int(5/6*steps)-1] @ phi.T
-# ax_C1R1[4,0].plot(V, tmp[0,:])
-# ax_C1R1[4,1].plot(V, tmp[1,:])
-
-# tmp = A_C1R1[int(6/6*steps)-1] @ phi.T
-# ax_C1R1[5,0].plot(V, tmp[0,:])
-# ax_C1R1[5,1].plot(V, tmp[1,:])
 
 
 plt.show()
