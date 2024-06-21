94d959c0f9c2fb9bc5aa579a7baad4bc9171bf00
Björn Volkmann <bjoern.volkmann@imes.uni-hannover.de>
2024-06-21 16:02:37 +0200

    changed visualization to new template

--- SingleMassOscillator_APF.py
+++ SingleMassOscillator_APF.py
@@ -4,8 +4,9 @@ import jax
 import jax.numpy as jnp
 import functools
 
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+
+
+import matplotlib.pyplot as plt
 
 
 
@@ -13,7 +14,7 @@ from src.SingleMassOscillator import F_spring, F_damper, f_x, N_ip, ip, H, m
 from src.BayesianInferrence import prior_mniw_2naturalPara, prior_mniw_2naturalPara_inv
 from src.BayesianInferrence import prior_mniw_updateStatistics, prior_mniw_CondPredictive
 from src.Filtering import systematic_SISR, squared_error
-from src.SingleMassOscillatorPlotting import generate_Animation
+from src.Publication_Plotting import plot_BFE_2D, generate_BFE_TimeSlices, plot_Data, apply_basic_formatting
 
 
 
@@ -26,6 +27,7 @@ rng = np.random.default_rng()
 N = 200
 t_end = 100.0
 dt = 0.01
+forget_factor = 0.999
 time = np.arange(0.0,t_end,dt)
 steps = len(time)
 
@@ -72,7 +74,10 @@ F_sd = np.zeros((steps,))
 Sigma_X = np.zeros((steps,N,2)) # filter
 Sigma_F = np.zeros((steps,N))
 
-A_Pred = np.zeros((steps, N_ip)) # GP
+Mean_F = np.zeros((steps, 1, N_ip)) # GP
+Col_cov_F = np.zeros((steps, N_ip, N_ip)) # GP
+Row_scale_F = np.zeros((steps, 1, 1)) # GP
+df_F = np.zeros((steps,)) # GP
 
 N_eff = np.zeros((steps,)) # effective sample size
 
@@ -115,10 +120,10 @@ for i in tqdm(range(1,steps), desc="Running simulation"):
     ### Step 1: Propagate GP parameters in time
     
     # apply forgetting operator to statistics for t-1 -> t
-    GP_model_stats[0] *= 0.999
-    GP_model_stats[1] *= 0.999
-    GP_model_stats[2] *= 0.999
-    GP_model_stats[3] *= 0.999
+    GP_model_stats[0] *= forget_factor
+    GP_model_stats[1] *= forget_factor
+    GP_model_stats[2] *= forget_factor
+    GP_model_stats[3] *= forget_factor
     
     # calculate parameters of GP from prior and sufficient statistics
     GP_para = list(jax.vmap(prior_mniw_2naturalPara_inv)(
@@ -137,8 +142,8 @@ for i in tqdm(range(1,steps), desc="Running simulation"):
     x_aux = jax.vmap(
         functools.partial(f_x, F=F[i-1], dt=dt)
         )(
-            x=Sigma_X[i-1,...], 
-            F_sd=Sigma_F[i-1,...]
+            x=Sigma_X[i-1], 
+            F_sd=Sigma_F[i-1]
             )
     
     # calculate first stage weights
@@ -156,16 +161,6 @@ for i in tqdm(range(1,steps), desc="Running simulation"):
     idx = np.array(systematic_SISR(u, p))
     idx[idx >= N] = N - 1 # correct out of bounds indices from numerical errors
     
-    # copy statistics
-    GP_model_stats[0] = GP_model_stats[0][idx,...]
-    GP_model_stats[1] = GP_model_stats[1][idx,...]
-    GP_model_stats[2] = GP_model_stats[2][idx,...]
-    GP_model_stats[3] = GP_model_stats[3][idx,...]
-    GP_para[0] = GP_para[0][idx,...]
-    GP_para[1] = GP_para[1][idx,...]
-    GP_para[2] = GP_para[2][idx,...]
-    GP_para[3] = GP_para[3][idx,...]
-    
     
     
     ### Step 3: Make a proposal by generating samples from the hirachical 
@@ -176,23 +171,23 @@ for i in tqdm(range(1,steps), desc="Running simulation"):
     Sigma_X[i] = jax.vmap(
         functools.partial(f_x, F=F[i-1], dt=dt)
         )(
-            x=Sigma_X[i-1,idx,:], 
+            x=Sigma_X[i-1,idx], 
             F_sd=Sigma_F[i-1,idx]
     ) + w_x
     
     ## sample from proposal for F at time t
     # evaluate basis functions for all particles
-    phi_x0 = jax.vmap(H)(Sigma_X[i-1,idx,:])
+    phi_x0 = jax.vmap(H)(Sigma_X[i-1,idx])
     phi_x1 = jax.vmap(H)(Sigma_X[i])
     
     # calculate conditional predictive distribution
     c_mean, c_col_scale, c_row_scale, df = jax.vmap(
         functools.partial(prior_mniw_CondPredictive, y1_var=3e-2)
         )(
-            mean=GP_para[0],
-            col_cov=GP_para[1],
-            row_scale=GP_para[2],
-            df=GP_para[3],
+            mean=GP_para[0][idx],
+            col_cov=GP_para[1][idx],
+            row_scale=GP_para[2][idx],
+            df=GP_para[3][idx],
             y1=Sigma_F[i-1,idx],
             basis1=phi_x0,
             basis2=phi_x1
@@ -206,7 +201,10 @@ for i in tqdm(range(1,steps), desc="Running simulation"):
     
     # Update the sufficient statistics of GP with new proposal
     GP_model_stats = list(jax.vmap(prior_mniw_updateStatistics)(
-        *GP_model_stats,
+        GP_model_stats[0][idx],
+        GP_model_stats[1][idx],
+        GP_model_stats[2][idx],
+        GP_model_stats[3][idx],
         Sigma_F[i],
         phi_x1
     ))
@@ -227,7 +225,10 @@ for i in tqdm(range(1,steps), desc="Running simulation"):
         GP_model_prior_eta[2] + np.einsum('n...,n->...', GP_model_stats[2], weights[i]),
         GP_model_prior_eta[3] + np.einsum('n...,n->...', GP_model_stats[3], weights[i])
     )
-    A_Pred[i,...] = GP_para_logging[0]
+    Mean_F[i] = GP_para_logging[0]
+    Col_cov_F[i] = GP_para_logging[1]
+    Row_scale_F[i] = GP_para_logging[2]
+    df_F[i] = GP_para_logging[3]
     
     #abort
     if np.any(np.isnan(weights[i])):
@@ -239,61 +240,67 @@ for i in tqdm(range(1,steps), desc="Running simulation"):
 ################################################################################
 # Plots
 
-# genearte animation
-fig = generate_Animation(X, Y, F_sd, Sigma_X, Sigma_F, weights, H, A_Pred, time, 200., 30., 30)
-fig.show()
 
-# plot distribution of F_sd
-fig = make_subplots()
-time = time[0:-1:5]
-N_eff = N_eff[0:-1:5]
-Sigma_F = Sigma_F[0:-1:5,0:-1:5]
-weights = weights[0:-1:5,0:-1:5]
-weights = weights/np.sum(weights, axis=-1, keepdims=True)
-for i in range(weights.shape[-1]):
-    fig.add_trace(
-        go.Scatter(
-            x=time,
-            y=Sigma_F[:,i],
-            mode='markers',
-            marker={
-                'color': 'blue', 
-                'size':8,
-                'opacity': weights[:,i]
-            }
-        )
-    )
-fig.update_layout(showlegend=False)
-fig.show()
-
-# plot a single sample of F_sd over time
-fig = make_subplots()
-fig.add_trace(
-    go.Scatter(
-        x=time,
-        y=Sigma_F[:,0],
-        mode='lines',
-        line={
-            'color': 'blue', 
-            'width':3
-        }
-    )
+# plot the state estimations
+fig_X, axes_X = plot_Data(
+    Particles=Sigma_X,
+    weights=weights,
+    Reference=X,
+    time=time
 )
-fig.update_layout(showlegend=False)
-fig.show()
+apply_basic_formatting(fig_X)
 
-# plot effective sample size over time
-fig = make_subplots()
-fig.add_trace(
-    go.Scatter(
-        x=time,
-        y=N_eff,
-        mode='lines',
-        line={
-            'color': 'blue', 
-            'width':3
-        }
+
+# # plot the force estimations
+# fig_F, axes_F = plot_Data(
+#     Particles=Sigma_F,
+#     weights=weights,
+#     Reference=F,
+#     time=time
+# )
+# apply_basic_formatting(fig_F)
+
+
+
+# plot time slices of the learned spring-damper function
+x_plt = np.linspace(-5., 5., 50)
+dx_plt = np.linspace(-5., 5., 50)
+grid_x, grid_y = np.meshgrid(x_plt, dx_plt, indexing='xy')
+X_in = np.vstack([grid_x.flatten(), grid_y.flatten()]).T
+
+F_sd_true = jax.vmap(F_spring)(X_in[:,0]) + jax.vmap(F_damper)(X_in[:,1])
+
+Mean, Std, X_stats, X_weights, Time = generate_BFE_TimeSlices(
+    N_slices=4, 
+    X_in=X_in, 
+    Sigma_X=Sigma_X[:int(steps*3/5)], 
+    Sigma_weights=weights[:int(steps*3/5)],
+    Mean=Mean_F[:int(steps*3/5)], 
+    Col_Scale=Col_cov_F[:int(steps*3/5)], 
+    Row_Scale=Row_scale_F[:int(steps*3/5)], 
+    DF=df_F[:int(steps*3/5)], 
+    basis_fcn=H, 
+    forget_factor=forget_factor
     )
+
+log_mean_err = np.log10(np.abs(Mean-F_sd_true))
+fig_BFE_F, ax_BFE_F = plot_BFE_2D(
+    X_in,
+    log_mean_err,
+    Time*dt,
+    X_stats, 
+    X_weights
 )
-fig.update_layout(showlegend=False)
-fig.show()
+
+
+# set x label
+for ax in ax_BFE_F[1]:
+    ax.set_xlabel(r'$\dot{s}$ in [m/s]')
+ax_BFE_F[0,0].set_ylabel(r'$s$ in [m]')
+ax_BFE_F[1,0].set_ylabel(r'$s$ in [m]')
+    
+apply_basic_formatting(fig_BFE_F)
+
+
+
+plt.show()
