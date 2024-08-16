21c2ba811f5882d62ae4e6746d6e624a0f8f3031
Björn Volkmann <bjoern.volkmann@imes.uni-hannover.de>
2024-08-16 10:10:26 +0200

    PGAS for Battery can run now

--- Battery_PGAS.py
+++ Battery_PGAS.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 
 from src.Battery import Battery_CPFAS_Kernel, basis_fcn, scale_C1, scale_R1
 from src.Battery import steps, N_basis_fcn, GP_prior_C1R1, offset_C1, offset_R1
+from src.Battery import  forget_factor
 from src.Publication_Plotting import apply_basic_formatting, plot_fcn_error_1D
 from src.BayesianInferrence import prior_mniw_2naturalPara_inv
 from src.BayesianInferrence import prior_mniw_calcStatistics
@@ -18,14 +19,11 @@ from src.BayesianInferrence import prior_mniw_Predictive
 
 
 ################################################################################
-# Model
+### Offline Algorithm
 
-
-### Online Algorithm
 N_iterations = 2
-Sigma_X = np.zeros((steps,N_iterations,2))
+Sigma_X = np.zeros((steps,N_iterations))
 Sigma_C1R1 = np.zeros((steps,N_iterations,2))
-Likelihood_Y = np.zeros((N_iterations,))
 weights = np.ones((steps,N_iterations))/N_iterations
 
 # logging of model
@@ -36,6 +34,12 @@ df_C1R1 = np.zeros((N_iterations,)) # GP
 
 # variable for sufficient statistics
 GP_model_stats = [
+    np.zeros((N_basis_fcn, 2)),
+    np.zeros((N_basis_fcn, N_basis_fcn)),
+    np.zeros((2, 2)),
+    0
+]
+GP_model_stats_logging = [
     np.zeros((N_basis_fcn, 2)),
     np.zeros((N_basis_fcn, N_basis_fcn)),
     np.zeros((2, 2)),
@@ -132,6 +136,11 @@ for k in range(1, N_iterations):
     Col_Cov_C1R1[k] = GP_para[1]
     Row_Scale_C1R1[k] = GP_para[2]
     df_C1R1[k] = GP_para[3]
+    
+    GP_model_stats_logging[0] = GP_model_stats_logging[0] * forget_factor + np.sum(T_0, axis=0)
+    GP_model_stats_logging[1] = GP_model_stats_logging[0] * forget_factor + np.sum(T_1, axis=0)
+    GP_model_stats_logging[2] = GP_model_stats_logging[0] * forget_factor + np.sum(T_2, axis=0)
+    GP_model_stats_logging[3] = GP_model_stats_logging[0] * forget_factor + np.sum(T_3, axis=0)
 
 
 
@@ -144,12 +153,12 @@ V = jnp.linspace(0, 2.2, 500)
 basis_in = jax.vmap(basis_fcn)(V)
 
 N_slices = 1
-index = (np.array(range(N_slices))+1)/N_slices*(steps-1)
+index = (np.array(range(N_slices))+1)/N_slices*(N_iterations-1)
 
 # function value from GP
-fcn_mean = np.zeros((steps, 2, V.shape[0]))
-fcn_var = np.zeros((steps, 2, V.shape[0]))
-for i in tqdm(range(0, steps), desc='Calculating fcn value and var'):
+fcn_mean = np.zeros((N_iterations, 2, V.shape[0]))
+fcn_var = np.zeros((N_iterations, 2, V.shape[0]))
+for i in tqdm(range(0, N_iterations), desc='Calculating fcn value and var'):
     mean, col_scale, row_scale, _ = prior_mniw_Predictive(
         mean=Mean_C1R1[i], 
         col_cov=Col_Cov_C1R1[i], 
@@ -158,7 +167,7 @@ for i in tqdm(range(0, steps), desc='Calculating fcn value and var'):
         basis=basis_in)
     fcn_var[i,0,:] = np.diag(col_scale-1) * row_scale[0,0] * scale_C1**2
     fcn_var[i,1,:] = np.diag(col_scale-1) * row_scale[1,1] * scale_R1**2
-    fcn_mean[i] = (mean * np.array([scale_C1, scale_R1]) + np.array([offset_C1, offset_R1])).T
+    fcn_mean[i] = ((mean + np.array([offset_C1, offset_R1])) * np.array([scale_C1, scale_R1])).T
 
 # generate plot
 for i in index:
@@ -168,9 +177,9 @@ for i in index:
         Std=np.sqrt(fcn_var[int(i)]),
         X_stats=Sigma_X[:int(i)], 
         X_weights=weights[:int(i)])
-    ax_fcn_e[0][2].set_xlabel(r"Voltage in $\mathrm{V}$")
-    ax_fcn_e[0][1].set_ylabel(r"$C_1$ in $\mathrm{F}$")
-    ax_fcn_e[0][2].set_ylabel(r"$R_1$ in $\mathrm{\Omega}$")
+    ax_fcn_e[0][1].set_xlabel(r"Voltage in $\mathrm{V}$")
+    ax_fcn_e[0][0].set_ylabel(r"$C_1$ in $\mathrm{F}$")
+    ax_fcn_e[0][1].set_ylabel(r"$R_1$ in $\mathrm{\Omega}$")
         
     apply_basic_formatting(fig_fcn_e, width=8, aspect_ratio=1, font_size=8)
     fig_fcn_e.savefig(f"Battery_PGAS_C1R1_fcn_{int(i)}.svg")
