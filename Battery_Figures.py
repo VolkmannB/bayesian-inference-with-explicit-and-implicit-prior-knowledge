2b25d821bab961b40a02154701ea16315bcf96d4
Björn Volkmann <bjoern.volkmann@imes.uni-hannover.de>
2024-08-22 16:30:15 +0200

    Finished plotting

--- Battery_Figures.py
+++ Battery_Figures.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 from src.Battery import scale_C1, scale_R1
 from src.Battery import offset_C1, offset_R1, steps
-from src.Publication_Plotting import plot_fcn_error_1D
+from src.Publication_Plotting import plot_fcn_error_1D, imes_blue, imes_orange
 from src.Publication_Plotting import apply_basic_formatting, plot_Data
 from src.BayesianInferrence import prior_mniw_Predictive, prior_mniw_2naturalPara_inv
 
@@ -64,6 +64,34 @@ del data
     GP_prior_stats[2] + np.cumsum(offline_T2, axis=0),
     GP_prior_stats[3] + np.cumsum(offline_T3, axis=0)
 )
+del offline_T0, offline_T1, offline_T2, offline_T3
+    
+# Convert sufficient statistics to standard parameters
+(online_GP_Mean, online_GP_Col_Cov, 
+ online_GP_Row_Scale, online_GP_df) = jax.vmap(prior_mniw_2naturalPara_inv)(
+    GP_prior_stats[0] + online_T0,
+    GP_prior_stats[1] + online_T1,
+    GP_prior_stats[2] + online_T2,
+    GP_prior_stats[3] + online_T3
+)
+del online_T0, online_T1, online_T2, online_T3
+
+
+# function values with GP prior
+GP_prior = prior_mniw_2naturalPara_inv(
+            GP_prior_stats[0],
+            GP_prior_stats[1],
+            GP_prior_stats[2],
+            GP_prior_stats[3]
+        )
+_, col_scale_prior, row_scale_prior, _ = prior_mniw_Predictive(
+    mean=GP_prior[0], 
+    col_cov=GP_prior[1], 
+    row_scale=GP_prior[2], 
+    df=GP_prior[3], 
+    basis=basis_plot)
+fcn_var_prior = np.diag(col_scale_prior-1) * row_scale_prior[0,0]
+del col_scale_prior, row_scale_prior, GP_prior
 
 
 
@@ -86,8 +114,8 @@ N_PGAS_iter = offline_Sigma_X.shape[1]
 index = (np.array(range(N_slices))+1)/N_slices*(N_PGAS_iter-1)
 
 # function value from GP
-fcn_mean = np.zeros((N_PGAS_iter, 2, X_plot.shape[0]))
-fcn_var = np.zeros((N_PGAS_iter, 2, X_plot.shape[0]))
+fcn_mean_offline = np.zeros((N_PGAS_iter, 2, X_plot.shape[0]))
+fcn_var_offline = np.zeros((N_PGAS_iter, 2, X_plot.shape[0]))
 for i in tqdm(range(0, N_PGAS_iter), desc='Calculating fcn value and var'):
     mean, col_scale, row_scale, _ = prior_mniw_Predictive(
         mean=offline_GP_Mean[i], 
@@ -95,16 +123,16 @@ for i in tqdm(range(0, N_PGAS_iter), desc='Calculating fcn value and var'):
         row_scale=offline_GP_Row_Scale[i], 
         df=offline_GP_df[i], 
         basis=basis_plot)
-    fcn_var[i,0,:] = np.diag(col_scale-1) * row_scale[0,0] * scale_C1**2
-    fcn_var[i,1,:] = np.diag(col_scale-1) * row_scale[1,1] * scale_R1**2
-    fcn_mean[i] = ((mean + np.array([offset_C1, offset_R1])) * np.array([scale_C1, scale_R1])).T
+    fcn_var_offline[i,0,:] = np.diag(col_scale-1) * row_scale[0,0] * scale_C1**2
+    fcn_var_offline[i,1,:] = np.diag(col_scale-1) * row_scale[1,1] * scale_R1**2
+    fcn_mean_offline[i] = ((mean + np.array([offset_C1, offset_R1])) * np.array([scale_C1, scale_R1])).T
 
 # generate plot
 for i in index:
     fig_fcn_e, ax_fcn_e = plot_fcn_error_1D(
         X_plot, 
-        Mean=fcn_mean[int(i)], 
-        Std=np.sqrt(fcn_var[int(i)]),
+        Mean=fcn_mean_offline[int(i)], 
+        Std=np.sqrt(fcn_var_offline[int(i)]),
         X_stats=offline_Sigma_X[:,:int(i)], 
         X_weights=offline_weights[:,:int(i)])
     ax_fcn_e[0][1].set_xlabel(r"Voltage in $\mathrm{V}$")
@@ -113,6 +141,93 @@ for i in index:
         
     apply_basic_formatting(fig_fcn_e, width=8, aspect_ratio=1, font_size=8)
     fig_fcn_e.savefig(f"Battery_PGAS_C1R1_fcn_{int(i)}.svg")
+
+
+
+################################################################################
+# Plotting Online
+
+# plot the state estimations
+fig_X, axes_X = plot_Data(
+    Particles=online_Sigma_Y[...,None],
+    weights=online_weights,
+    Reference=Y,
+    time=time
+)
+axes_X[0].set_ylabel(r"$V$ in $\mathrm{V}$")
+axes_X[0].set_xlabel(r"Time in $\mathrm{s}$")
+apply_basic_formatting(fig_X, width=10, aspect_ratio=0.6, font_size=8)
+fig_X.savefig("Battery_APF_Y.svg", bbox_inches='tight')
+
+steps = time.shape[0]
+index = (np.array(range(N_slices))+1)/N_slices*(steps-1)
+
+# function value from GP
+fcn_mean_online = np.zeros((steps, 2, X_plot.shape[0]))
+fcn_var_online = np.zeros((steps, 2, X_plot.shape[0]))
+for i in tqdm(range(0, steps), desc='Calculating fcn value and var'):
+    mean, col_scale, row_scale, _ = prior_mniw_Predictive(
+        mean=online_GP_Mean[i], 
+        col_cov=online_GP_Col_Cov[i], 
+        row_scale=online_GP_Row_Scale[i], 
+        df=online_GP_df[i], 
+        basis=basis_plot)
+    fcn_var_online[i,0,:] = np.diag(col_scale-1) * row_scale[0,0] * scale_C1**2
+    fcn_var_online[i,1,:] = np.diag(col_scale-1) * row_scale[1,1] * scale_R1**2
+    fcn_mean_online[i] = ((mean + np.array([offset_C1, offset_R1])) * np.array([scale_C1, scale_R1])).T
+
+# generate plot
+for i in index:
+    fig_fcn_e, ax_fcn_e = plot_fcn_error_1D(
+        X_plot, 
+        Mean=fcn_var_online[int(i)], 
+        Std=np.sqrt(fcn_var_online[int(i)]),
+        X_stats=online_Sigma_X[:int(i)], 
+        X_weights=online_weights[:int(i)])
+    ax_fcn_e[0][1].set_xlabel(r"Voltage in $\mathrm{V}$")
+    ax_fcn_e[0][0].set_ylabel(r"$C_1$ in $\mathrm{F}$")
+    ax_fcn_e[0][1].set_ylabel(r"$R_1$ in $\mathrm{\Omega}$")
+        
+    apply_basic_formatting(fig_fcn_e, width=8, aspect_ratio=1, font_size=8)
+    fig_fcn_e.savefig(f"Battery_APF_C1R1_fcn_{int(i)}.svg")
+    
+
+
+# plot weighted RMSE of GP over entire function space
+fcn_var = fcn_var_online + fcn_var_offline[-1]
+w = 1 / fcn_var[:,0,:]
+w = w / np.sum(w, axis=-1, keepdims=True)
+v1 = np.sum(w, axis=-1)
+v2 = np.sum(w**2, axis=-1)
+wRMSE_f = np.sqrt(1/(v1-(v2/v1**2)) * np.sum((fcn_mean_online[:,0] - fcn_mean_offline[-1,0])**2 * w, axis=-1))
+
+w = 1 / fcn_var[:,1,:]
+w = w / np.sum(w, axis=-1, keepdims=True)
+v1 = np.sum(w, axis=-1)
+v2 = np.sum(w**2, axis=-1)
+wRMSE_r = np.sqrt(1/(v1-(v2/v1**2)) * np.sum((fcn_mean_online[:,1] - fcn_mean_offline[-1,1])**2 * w, axis=-1))
+
+fig_RMSE, ax_RMSE = plt.subplots(1,1, layout='tight')
+ax_RMSE.plot(
+    time,
+    wRMSE_f,
+    color=imes_blue
+)
+ax_RMSE.plot(
+    time,
+    wRMSE_r,
+    color=imes_orange
+)
+ax_RMSE.set_ylabel(r"wRMSE")
+ax_RMSE.set_xlabel(r"Time in $\mathrm{s}$")
+ax_RMSE.set_ylim(0)
+
+for i in index:
+    ax_RMSE.plot([time[int(i)], time[int(i)]], [0, wRMSE_f[int(i)]*1.5], color="black", linewidth=0.8)
+    
+    
+apply_basic_formatting(fig_RMSE, width=8, font_size=8)
+fig_RMSE.savefig("Vehicle_APF_C1R1_wRMSE.svg", bbox_inches='tight')
 
 
 
