817b270540ae8771583013077e94c292fe5ca6b7
Björn Volkmann <bjoern.volkmann@imes.uni-hannover.de>
2024-10-16 16:01:29 +0200

    changed layout for offline results found error on post processing offline results

--- EMPS_Figures.py
+++ EMPS_Figures.py
@@ -59,7 +59,7 @@ del data
     GP_prior_stats[0] + np.cumsum(offline_T0, axis=0)/np.arange(1,offline_Sigma_X.shape[1]+1)[:,None,None],
     GP_prior_stats[1] + np.cumsum(offline_T1, axis=0)/np.arange(1,offline_Sigma_X.shape[1]+1)[:,None,None],
     GP_prior_stats[2] + np.cumsum(offline_T2, axis=0)/np.arange(1,offline_Sigma_X.shape[1]+1)[:,None,None],
-    GP_prior_stats[3] + np.cumsum(offline_T3, axis=0)/np.arange(1,offline_Sigma_X.shape[1]+1)[:,None,None]
+    GP_prior_stats[3] + np.cumsum(offline_T3, axis=0)/np.arange(1,offline_Sigma_X.shape[1]+1)
 )
 del offline_T0, offline_T1, offline_T2, offline_T3
     
