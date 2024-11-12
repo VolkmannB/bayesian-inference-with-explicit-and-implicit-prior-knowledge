fixed error in calculation of MNIW base measure

src/BayesianInferrence.py
@@ -30,7 +30,7 @@ def prior_mniw_2naturalPara(mean, col_cov, row_scale, df):
     eta_1 = temp[:,mean.shape[0]:]
     eta_2 = mean @ eta_0 + row_scale
     
-    eta_3 = df + col_cov.shape[0] + row_scale.shape[0] + 1
+    eta_3 = df
     
     return eta_0, eta_1, eta_2, eta_3
 
@@ -45,7 +45,7 @@ def prior_mniw_2naturalPara_inv(eta_0, eta_1, eta_2, eta_3):
     mean = temp[:,:eta_0.shape[1]].T
     col_cov = temp[:,eta_0.shape[1]:]
     row_scale = eta_2 - mean @ eta_0
-    df = eta_3 - col_cov.shape[0] - row_scale.shape[0] - 1
+    df = eta_3
     
     return jnp.atleast_2d(mean), col_cov, jnp.atleast_2d(row_scale), df
 
@@ -129,12 +129,16 @@ def prior_mniw_log_base_measure(T_0, T_1, T_2, T_3):
     n = T_2.shape[0]
     m = T_1.shape[0]
     
+    Psi = T_2 - T_0.T @ jnp.linalg.solve(T_1, T_0)
+    nu = T_3
+    
     temp_1 = - 0.5 * n * m * jnp.log(2*jnp.pi)
-    temp_2 = 0.5 * m * jnp.log(jnp.linalg.det(T_1))
-    temp_3 = - 0.5 * (T_3 - n -m - 1) * jnp.log(2)
-    temp_4 = - jsc.special.multigammaln((T_3 - n -m - 1)/2, n)
+    temp_2 = 0.5 * n * jnp.log(jnp.linalg.det(T_1))
+    temp_3 = - 0.5 * nu * n * jnp.log(2)
+    temp_4 = - jsc.special.multigammaln(nu/2, n)
+    temp_5 = jnp.log(jnp.linalg.det(Psi)) * nu / 2
 
-    return temp_1 + temp_2 + temp_3 + temp_4
+    return temp_1 + temp_2 + temp_3 + temp_4 + temp_5
 
     
 

src/EMPS.py
@@ -51,7 +51,7 @@ rng = np.random.default_rng(16723573)
 # sim para
 N_particles = 200
 N_PGAS_iter = 800
-forget_factor = 1 - 1/700
+forget_factor = 0.999
 
 ### Load data
 data = scipy.io.loadmat('src\Measurements\DATA_EMPS.mat')
@@ -92,16 +92,14 @@ ctrl_input = (data['vir'] * data['gtau']).flatten()[0:-1:10]
 
 # parameters
 M = 95.11
-F_c = 20.39
-OFF = -3.16
 
 
 # state dynamics
-def dx(x, tau, F_v, F_c=F_c, M=M, OFF=OFF):
+def dx(x, tau, F):
         
         # x = [q, dq]
         dq = x[1]
-        ddq = (tau - F_v)/M
+        ddq = (tau - F)/M
     
         return jnp.array([dq, ddq])
 
@@ -484,10 +482,10 @@ def EMPS_CPFAS_Kernel(x_ref, F_ref, GP_stats_ref, Y=Y):
         
         # calculate models
         Mean, Col_Cov, Row_Scale, df = jax.vmap(prior_mniw_2naturalPara_inv)(
-            GP_prior[0] + GP_stats_ref[0] + GP_stats_ancestor[0],
-            GP_prior[1] + GP_stats_ref[1] + GP_stats_ancestor[1],
-            GP_prior[2] + GP_stats_ref[2] + GP_stats_ancestor[2],
-            GP_prior[3] + GP_stats_ref[3] + GP_stats_ancestor[3],
+            GP_prior[0] + GP_stats_ancestor[0],
+            GP_prior[1] + GP_stats_ancestor[1],
+            GP_prior[2] + GP_stats_ancestor[2],
+            GP_prior[3] + GP_stats_ancestor[3],
         )
         
         